5d5423f2579de3c1833aeee71e47764baf7e787f
AbdMamun1999 <mamunrox@gmail.com>
2022-02-17 23:17:31 +0600

    add script file

---
+++ js/script.js
@@ -0,0 +1,110 @@
+function getValueOfinputField(inputFieldId){
+    const inputField = document.getElementById(inputFieldId)
+    const inputFieldText = inputField.value;
+    let inputFieldNumber = parseInt(inputFieldText)
+    return inputFieldNumber;    
+}
+function calculate(){
+    const totalIncome = getValueOfinputField('income-field')
+    const foodExpense = getValueOfinputField('food-field')
+    const rentExpense = getValueOfinputField('rent-field')
+    const clothesExpense = getValueOfinputField('clothes-field');
+    // total expense id
+    const totalExpenseField = document.getElementById('totalEexpense-field')
+    // balance id
+    const balancedField = document.getElementById('balanced-field');
+//    update total Expense and balance
+    const totalExpense = foodExpense + rentExpense + clothesExpense;
+    totalExpenseField.innerText = totalExpense;
+    const balance = totalIncome - totalExpense;
+    balancedField.innerText = balance;
+// error handle total expense do not more than income
+    const errorMassageThree = document.getElementById('error-massageThree')
+    if(totalExpense>totalIncome) {
+        errorMassageThree.style.display = 'block'
+        balancedField.innerText = 'Balance-empty';
+    }
+}
+function calculateSavingsAndRemaining(){
+    const totalIncome = getValueOfinputField('income-field')
+    const foodExpense = getValueOfinputField('food-field')
+    const rentExpense = getValueOfinputField('rent-field')
+    const clothesExpense = getValueOfinputField('clothes-field')
+    const totalExpense = foodExpense + rentExpense + clothesExpense;
+    const balance = totalIncome - totalExpense;
+    // percent field 
+    const percentField = document.getElementById('percent-field');
+    const percentFieldNumber = parseInt(percentField.value)
+    // calculate savings
+    const savings = (totalIncome * percentFieldNumber)/100;
+    console.log(savings);
+      // update savings field
+    const savingsAmountField = document.getElementById('savingAmount-field')
+    // error massage for savings amount
+    const errorMassageFour = document.getElementById('error-massageFour')
+    const remainingBalance = document.getElementById('remainingBalance-field')
+    if(savings>balance){
+        errorMassageFour.style.display = 'block';
+        savingsAmountField.innerText = '0'
+        remainingBalance.innerText = '0'
+        
+    }
+    // update remaining balance
+    else{
+        savingsAmountField.innerText = savings;
+         remainingBalance.innerText = balance - savings;
+    }
+
+    
+}
+
+document.getElementById('calculate-button').addEventListener('click',function(){
+    const totalIncome = getValueOfinputField('income-field')
+    const foodExpense = getValueOfinputField('food-field')
+    const rentExpense = getValueOfinputField('rent-field')
+    const clothesExpense = getValueOfinputField('clothes-field')
+    // error massage
+    const errorMassageOne = document.getElementById('error-massageOne')
+    const errorMassageTwo = document.getElementById('error-massageTwo')
+    console.log(errorMassageOne)
+    if(totalIncome > 0){
+        if(foodExpense>0){
+            if(rentExpense>0){
+                if(clothesExpense>0){
+                    calculate()
+                }else{
+                    if(clothesExpense < 0){
+                        errorMassageOne.style.display = 'block'
+                    }else if(isNaN(clothesExpense)){
+                        errorMassageTwo.style.display = 'block'
+                    }
+                }
+            }else{
+                if(rentExpense < 0){
+                    errorMassageOne.style.display = 'block'
+                }else if(isNaN(rentExpense)){
+                    errorMassageTwo.style.display = 'block'
+                }
+            }
+        }else{
+            if(foodExpense < 0){
+                errorMassageOne.style.display = 'block'
+            }else if(isNaN(foodExpense)){
+                errorMassageTwo.style.display = 'block'
+            }
+        }
+    }
+    else{
+        if(totalIncome < 0){
+            errorMassageOne.style.display = 'block'
+        }else if(isNaN(totalIncome)){
+            errorMassageTwo.style.display = 'block'
+        }
+    }
+    
+    
+    
+})
+document.getElementById('save-button').addEventListener('click',function(){
+    calculateSavingsAndRemaining()
+})
